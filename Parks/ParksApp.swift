3a6f6a9fdd371173d12651e975b7cf4a7a7742e7
Michael Dacanay <michaeldacanayusa@gmail.com>
2024-03-18 21:20:33 -0400

    Initial Commit

---
+++ Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksApp.swift
+//  Parks
+//
+//  Created by Michael Dacanay on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
